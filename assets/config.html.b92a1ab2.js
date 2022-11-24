e5238d6daad252db739e69b6f06e8579704fa2bf
sena-nana <34237511+sena-nana@users.noreply.github.com>
2022-11-24 06:45:15 +0000

    Deploying to gh-pages from @ sena-nana/MutsukiDocs@7a6479255d1309b3b1fd8bc3a958daab40fcce96 🚀

---
+++ assets/config.html.b92a1ab2.js
@@ -0,0 +1,17 @@
+import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as i,c as n,e as s}from"./app.8070de6f.js";const d={},a=s(`<p>\u63D2\u4EF6\u7684\u8BBE\u7F6E\u5168\u90E8\u9700\u8981\u5728\u4F60\u7684\u9879\u76EE\u6839\u76EE\u5F55\u7684env\u6587\u4EF6\u4E2D\u5199\u597D(\u901A\u5E38\u662Fenv.dev)\u3002\u5728\u8FD0\u884C\u65F6\uFF0C\u63D2\u4EF6\u4F1A\u4E00\u6B21\u6027\u8BFB\u5165\uFF0C\u5982\u679C\u4F60\u9700\u8981\u66F4\u6539\u5168\u5C40\u914D\u7F6E\uFF0C\u5FC5\u987B\u91CD\u65B0\u542F\u52A8bot\u624D\u80FD\u591F\u66F4\u65B0\u8BBE\u7F6E\u3002</p><p>\u4E0B\u9762\u662F\u4E00\u4EFD\u914D\u7F6E\u6587\u4EF6\u7684\u793A\u4F8B\uFF0C\u5305\u542B\u4E86nonebot\u81EA\u5E26\u7684\u914D\u7F6E</p><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>HOST=127.0.0.1
+PORT=8081
+LOG_LEVEL=DEBUG
+FASTAPI_RELOAD=false
+
+SUPERUSERS=[&quot;&quot;]  # \u914D\u7F6E NoneBot \u8D85\u7EA7\u7528\u6237
+NICKNAME=[&quot;Mutsuki&quot;]  # \u914D\u7F6E\u673A\u5668\u4EBA\u7684\u6635\u79F0
+COMMAND_START=[&quot;/&quot;,&quot;&quot;]  # \u914D\u7F6E\u547D\u4EE4\u8D77\u59CB\u5B57\u7B26
+COMMAND_SEP=[&quot;#&quot;]  # \u914D\u7F6E\u547D\u4EE4\u5206\u5272\u5B57\u7B26
+
+NOVELAI_TOKEN=&quot;eyJhbGciOiJI...&quot;
+NOVELAI_TAG=&quot;loli,1 girl,cute,kawaii,&quot;
+NOVELAI_PAID=1
+NOVELAI_DAYLIMIT=0
+NOVELAI_MODE=&quot;naifu&quot;
+NOVELAI_SITE=&quot;127.0.0.1:6969&quot;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="\u5FC5\u8981\u8BBE\u7F6E" tabindex="-1"><a class="header-anchor" href="#\u5FC5\u8981\u8BBE\u7F6E" aria-hidden="true">#</a> \u5FC5\u8981\u8BBE\u7F6E</h2><p>\u5FC5\u8981\u8BBE\u7F6E\u662F\u7528\u6765\u8FDE\u63A5\u540E\u7AEF\u670D\u52A1\u5668\u7684\uFF0C\u5982\u679C\u4F60\u6CA1\u6709\u6B63\u786E\u914D\u7F6E\uFF0C\u90A3\u4E48\u63D2\u4EF6\u5C06\u65E0\u6CD5\u6B63\u5E38\u5DE5\u4F5C\u3002\u5982\u679C\u4F60\u4F7F\u7528\u8BE5\u63D2\u4EF6\u7684\u6269\u5C55\uFF0C\u4E5F\u5FC5\u987B\u6B63\u786E\u914D\u7F6E\u8FD9\u4E9B\u8BBE\u7F6E\u3002</p><h3 id="" tabindex="-1"><a class="header-anchor" href="#" aria-hidden="true">#</a></h3>`,6),t=[a];function l(u,o){return i(),n("div",null,t)}const v=e(d,[["render",l],["__file","config.html.vue"]]);export{v as default};
